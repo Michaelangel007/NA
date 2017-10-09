ed9ecd7f673b8c358ec9f72764a5479a4321d723
mlemmert <mark@6502workshop.com>
2017-10-08 21:54:04 -0500

    *updated mob tables *deleted unneeded files from source_code folder which were left over from past batch file driven backup restores

--- source_code/map.compression.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1' %1 %2 %3"
-REM PAUSE
-@ECHO ON
--- source_code/sbasm.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\Mark\AppData\Local\Programs\Python\Python35-32\python.exe C:\sbasm3\sbasm.py %1 %2 %3
